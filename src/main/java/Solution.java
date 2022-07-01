e747f486583db414d24098f702b0810711247a87
gson Liang <yuanyu90221@gmail.com>
2022-07-01 18:09:00 +0800

    init commit

---
+++ src/main/java/Solution.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Solution {
+  public boolean wordBreak(String s, List<String> wordDict) {
+     int sLen = s.length();
+     boolean[] dp = new boolean[sLen+1];
+     dp[sLen] = true;
+     for (int start = sLen - 1; start >=0; start--) {
+        for (String word : wordDict) {
+          if (start + word.length() <= sLen &&
+              s.substring(start, start+word.length()).equals(word)) {
+            dp[start]= dp[start+word.length()];
+          }
+        }
+     }
+     return dp[0];
+  }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+  final private Solution sol = new Solution();
+  @Test
+  void wordBreakExample1() {
+    assertTrue(sol.wordBreak("leetcode", List.of("leet", "code")));
+  }
+  @Test
+  void wordBreakExample2() {
+    assertTrue(sol.wordBreak("applepenapple", List.of("apple", "pen")));
+  }
+  @Test
+  void wordBreakExample3() {
+    assertFalse(sol.wordBreak("catsandog", List.of("cats","dog","sand","and","cat")));
+  }
+}
